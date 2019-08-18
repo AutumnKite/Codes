7c11b2861591821bd8b611ec0a6fdd41ca24557e
AutumnKite <1790397194@qq.com>
2019-08-18 09:52:30 +0800

    update

--- LibreOJ/6060 「2017 山东一轮集训 Day1 SDWC2018 Day1」Set/线性基/std.cpp
+++ LibreOJ/6060 「2017 山东一轮集训 Day1 SDWC2018 Day1」Set/线性基/std.cpp
@@ -1,8 +1,42 @@
 #include <cstdio>
 #include <cctype>
-int read(){
-	
+#include <cstring>
+#include <algorithm>
+long long read(){
+	register long long x = 0;
+	register char f = 1, ch = getchar();
+	for (; !isdigit(ch); ch = getchar()) if (ch == '-') f ^= 1;
+	for (; isdigit(ch); ch = getchar()) x = (x << 1) + (x << 3) + (ch ^ '0');
+	return f ? x : -x;
+}
+#define N 100005
+int n;
+long long sum, a[N], b[65], ans;
+void insert(long long x){
+	for (register int i = 60; ~i; --i)
+		if (!(sum >> i & 1))
+			if (x >> i & 1){
+				if (!b[i]) return b[i] = x, void(0);
+				x ^= b[i];
+			}
+	for (register int i = 60; ~i; --i)
+		if (sum >> i & 1)
+			if (x >> i & 1){
+				if (!b[i]) return b[i] = x, void(0);
+				x ^= b[i];
+			}
 }
 int main(){
-	
+	n = read();
+	for (register int i = 1; i <= n; ++i) a[i] = read(), sum ^= a[i];
+	for (register int i = 1; i <= n; ++i) insert(a[i]);
+	for (register int i = 60; ~i; --i)
+		if (!(sum >> i & 1))
+			if (!(ans >> i & 1))
+				ans ^= b[i];
+	for (register int i = 60; ~i; --i)
+		if (sum >> i & 1)
+			if (!(ans >> i & 1))
+				ans ^= b[i];
+	printf("%lld\n", sum ^ ans);
 }
