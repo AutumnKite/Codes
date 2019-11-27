7b9afb3ffca81d9975271b4bc6176a772a0067f9
AutumnKite <1790397194@qq.com>
2019-11-27 08:40:34 +0800

    update

--- Codeforces/559E Gerald and Path/DP/std.cpp
+++ Codeforces/559E Gerald and Path/DP/std.cpp
@@ -22,6 +22,7 @@ int main(){
 		a[i].x = read(), t = read(), a[i].l = a[i].x - t, a[i].r = a[i].x + t;
 		p[++m] = a[i].x, p[++m] = a[i].l, p[++m] = a[i].r;
 	}
+	std :: sort(a + 1, a + 1 + n);
 	std :: sort(p + 1, p + 1 + m);
 	m = std :: unique(p + 1, p + 1 + m) - p - 1;
 	for (register int i = 1; i <= n; ++i){
@@ -31,19 +32,17 @@ int main(){
 	}
 	f[n][a[n].l] = f[n][a[n].x] = p[a[n].r] - p[a[n].x];
 	for (register int i = n; i > 1; --i)
-		for (register int j = 0; j <= a[i].x; ++j){
+		for (register int j = 1; j <= m; ++j){
+			chkmx(f[i - 1][j], f[i][j]);
 			int mn = 1e9;
 			for (register int k = i - 1; k; --k){
-				int l = std :: min(a[k].x, mn);
-				if (a[i - 1].x <= a[k].r)
-					chkmx(f[k][l], f[i][j] + p[std :: min(a[k].r, j)] - p[l]);
+				int l = std :: min(a[k].x, mn), r = std :: min(a[k].r, j);
+				chkmx(f[k][l], f[i][j] + p[r] - p[l]);
 				mn = std :: min(mn, a[k].l);
 			}
-			mn = 1e9;
-			for (register int k = i - 1; k; --k)
-				if (a[k].x > a[i - 1].l) mn = std :: min(mn, a[k].l);
-			chkmx(f[i - 1][mn], f[i][j] + p[std :: min(a[i - 1].r, j)])
+			chkmx(f[i - 1][a[i - 1].l], f[i][j] + p[std :: min(a[i - 1].x, j)] - p[a[i - 1].l]);
 		}
 	int ans = 0;
-	for (register int i = 1; i <= n; ++i) ans = std :: max
+	for (register int i = 1; i <= m; ++i) chkmx(ans, f[1][i]);
+	printf("%d\n", ans);
 }
