efdd0ab08f13369aa916532ef1bcb1dba693d109
AutumnKite <1790397194@qq.com>
2020-02-15 16:15:18 +0800

    update IO.cpp

--- Templates/IO.cpp
+++ Templates/IO.cpp
@@ -2,22 +2,111 @@
 #include <cctype>
 #include <cstring>
 #include <algorithm>
-char buf[1 << 23], *ps = buf, *pt = buf;
-#define getchar() (ps == pt && (pt = (ps = buf) + fread(buf, 1, 1 << 23, stdin), ps == pt) ? EOF : *ps++)
-int read(){
-	register int x = 0;
-	register char f = 1, ch = getchar();
-	for (; !isdigit(ch); ch = getchar()) if (ch == '-') f ^= 1;
-	for (; isdigit(ch); ch = getchar()) x = (x << 1) + (x << 3) + (ch ^ '0');
-	return f ? x : -x;
+namespace fastIO{
+#define getchar() my_getchar()
+#define putchar(x) my_putchar(x)
+	static const int IN_BUF = 1 << 23, OUT_BUF = 1 << 23;
+	char buf[IN_BUF], *ps = buf, *pt = buf;
+	inline char my_getchar() {
+		return ps == pt && (pt = (ps = buf) + fread(buf, 1, IN_BUF, stdin), ps == pt) ? EOF : *ps++;
+	}
+	template<typename T> inline bool read(T &x){
+		char op = 0, ch = getchar();
+		for (x = 0; !isdigit(ch) && ch != EOF; ch = getchar()) if (ch == '-') op ^= 1;
+		if (ch == EOF) return false;
+		for (; isdigit(ch); ch = getchar()) x = x * 10 + (ch ^ '0');
+		if (ch != '.') return op ? x = -x : 0, true; else ch = getchar();
+		T t = 1;
+		for (; isdigit(ch); ch = getchar()) t /= 10, x += (ch ^ '0') * t;
+		return op ? x = -x : 0, true;
+	}
+	inline int reads(char *s){
+		int n = 0;
+		char ch = getchar();
+		for (; isspace(ch) && ch != EOF; ch = getchar()) ;
+		for (; !isspace(ch) && ch != EOF; ch = getchar()) s[n++] = ch;
+		return s[n] = '\0', n;
+	}
+	char pbuf[OUT_BUF], *pp = pbuf;
+	struct _flusher{ ~_flusher(){ fwrite(pbuf, 1, pp - pbuf, stdout); } } OutputFlusher;
+	inline void my_putchar(char x){
+		pp == pbuf + OUT_BUF ? fwrite(pbuf, 1, OUT_BUF, stdout), pp = pbuf : 0, *pp++ = x;
+	}
+	template<typename T> inline void print_(T x){
+		if (x == 0) return putchar('0'), void(0);
+		int cnt = 0, num[25];
+		for (x < 0 ? putchar('-'), x = -x : 0; x; x /= 10) num[++cnt] = x % 10;
+		while (cnt) putchar(num[cnt] ^ '0'), --cnt;
+	}
+	template<typename T> inline void print(T x, char ch = '\n'){
+		print_(x), putchar(ch);
+	}
+	inline void prints_(const char *s, int n = -1){
+		if (n == -1) n = strlen(s);
+		for (register int i = 0; i < n; ++i) putchar(s[i]);
+	}
+	inline void prints(const char *s, int n = -1, char ch = '\n'){
+		prints_(s, n), putchar(ch);
+	}
+	template<typename T> inline void printd_(T x, int n){
+		if (x < 0) x = -x, putchar('-');
+		long long a, b;
+		if (n == 0) return a = x + 0.5, print_(a), void(0);
+		a = x, x -= a;
+		while (n--) x *= 10;
+		b = x + 0.5;
+		print_(a), putchar('.'), print_(b);
+	}
+	template<typename T> inline void printd(T x, int n, char ch = '\n'){
+		printd_(x, n), putchar(ch);
+	}
 }
-char pbuf[1 << 23], *pp = pbuf;
-#define putchar(x) (pp == pbuf + (1 << 23) ? fwrite(pbuf, 1, 1 << 23, stdout), pp = pbuf : 0, *pp++ = x)
-struct __IO_flusher{ ~__IO_flusher(){ fwrite(pbuf, 1, pp - pbuf, stdout); } } IO_flusher;
-void print(int x, char ch = '\n'){
-	if (x == 0) return putchar('0'), putchar(ch), void(0);
-	int cnt = 0, num[25];
-	for (x < 0 ? putchar('-'), x = -x : 0; x; x /= 10) num[++cnt] = x % 10;
-	while (cnt) putchar(num[cnt] ^ '0'), --cnt;
-	putchar(ch);
+using namespace fastIO;
+void solve_string(){
+	int n;
+	char s[1005];
+	n = reads(s);
+	prints(s + 1, std::min(50, n - 1));
 }
+void solve_int(){
+	int a, b;
+	read(a), read(b), print(a + b);
+}
+void solve_double(){
+	double c;
+	int n;
+	read(c), read(n);
+	printd(c, n);
+}
+int main(){
+	int T;
+	read(T);
+	for (register int t = 1; t <= T; ++t) solve_string();
+	for (register int t = 1; t <= T; ++t) solve_int();
+	for (register int t = 1; t <= T; ++t) solve_double();
+}
+/*
+A test Data:
+
+Input:
+3
+abcded     	f
+aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+19260817 -19260817
+33445566 -92758436
+-348935545 -358949545
+155.255949646856 1
+-0.259213 5
+-998.23456789015 7
+
+Output:
+bcded
+
+aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+0
+-59312870
+-707885090
+155.3
+-0.25921
+-998.2345679
+*/
