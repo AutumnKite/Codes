1135328dc14c43041c2b03271c5de398d31251f9
AutumnKite <1790397194@qq.com>
2020-06-18 21:12:34 +0800

    update

---
+++ Codeforces/1182F Maximum Sine/std.cpp
@@ -0,0 +1,139 @@
+#include <cstdio>
+#include <cctype>
+#include <cstring>
+#include <algorithm>
+#include <vector>
+
+#define debug(...) fprintf(stderr, __VA_ARGS__)
+
+#ifndef AT_HOME
+#define getchar() IO::myGetchar()
+#define putchar(x) IO::myPutchar(x)
+#endif
+
+namespace IO {
+	static const int IN_BUF = 1 << 23, OUT_BUF = 1 << 23;
+
+	inline char myGetchar() {
+		static char buf[IN_BUF], *ps = buf, *pt = buf;
+		if (ps == pt) {
+			ps = buf, pt = buf + fread(buf, 1, IN_BUF, stdin);
+		}
+		return ps == pt ? EOF : *ps++;
+	}
+
+	template<typename T>
+	inline bool read(T &x) {
+		bool op = 0;
+		char ch = getchar();
+		x = 0;
+		for (; !isdigit(ch) && ch != EOF; ch = getchar()) {
+			op ^= (ch == '-');
+		}
+		if (ch == EOF) {
+			return false;
+		}
+		for (; isdigit(ch); ch = getchar()) {
+			x = x * 10 + (ch ^ '0');
+		}
+		if (op) {
+			x = -x;
+		}
+		return true;
+	}
+
+	inline int readStr(char *s) {
+		int n = 0;
+		char ch = getchar();
+		for (; isspace(ch) && ch != EOF; ch = getchar())
+			;
+		for (; !isspace(ch) && ch != EOF; ch = getchar()) {
+			s[n++] = ch;
+		}
+		s[n] = '\0';
+		return n;
+	}
+
+	inline void myPutchar(char x) {
+		static char pbuf[OUT_BUF], *pp = pbuf;
+		struct _flusher {
+			~_flusher() {
+				fwrite(pbuf, 1, pp - pbuf, stdout);
+			}
+		};
+		static _flusher outputFlusher;
+		if (pp == pbuf + OUT_BUF) {
+			fwrite(pbuf, 1, OUT_BUF, stdout);
+			pp = pbuf;
+		}
+		*pp++ = x;
+	}
+
+	template<typename T>
+	inline void print_(T x) {
+		if (x == 0) {
+			putchar('0');
+			return;
+		}
+		std::vector<int> num;
+		if (x < 0) {
+			putchar('-');
+			x = -x;
+		}
+		for (; x; x /= 10) {
+			num.push_back(x % 10);
+		}
+		while (!num.empty()) {
+			putchar(num.back() ^ '0');
+			num.pop_back();
+		}
+	}
+
+	template<typename T>
+	inline void print(T x, char ch = '\n') {
+		print_(x);
+		putchar(ch);
+	}
+
+	inline void printStr_(const char *s, int n = -1) {
+		if (n == -1) {
+			n = strlen(s);
+		}
+		for (int i = 0; i < n; ++i) {
+			putchar(s[i]);
+		}
+	}
+
+	inline void printStr(const char *s, int n = -1, char ch = '\n') {
+		printStr_(s, n);
+		putchar(ch);
+	}
+}
+using namespace IO;
+
+int l, r, a, P;
+int B;
+std::pair<int, int> p[35005];
+
+void solve() {
+	read(l), read(r), read(a), read(P);
+	P *= 2, a = 2 * a % P;
+	B = std::sqrt(P);
+	for (int i = 1; i <= B; ++i) {
+		p[i].first = (P / 2 + 1ll * a * i) % P;
+		p[i].second = i;
+	}
+	std::sort(p + 1, p + 1 + B);
+	for (int i = 1; i <= P / B + 1; ++i) {
+		int w = 1ll * a * B % P * i % P;
+		
+	}
+}
+
+int main() {
+	int T = 1;
+	read(T);
+	while (T--) {
+		solve();
+	}
+}
