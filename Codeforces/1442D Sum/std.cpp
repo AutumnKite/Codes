6b9e0ab6c47a96d4ea411975a39374776f14a57b
AutumnKite <1790397194@qq.com>
2021-05-29 16:23:52 +0800

    update

---
+++ Codeforces/1442D Sum/std.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+int main() {
+	std::ios_base::sync_with_stdio(false);
+	std::cin.tie(0);
+
+	int n, m;
+	std::cin >> n >> m;
+	std::vector<std::vector<int>> a(n);
+	for (int i = 0; i < n; ++i) {
+
+	}
+}
