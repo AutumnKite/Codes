35f37814d14b1c984b9872158d2f81762e24b860
AutumnKite <1790397194@qq.com>
2021-06-01 22:46:00 +0800

    update

--- Codeforces/1385G Columns Swaps/std.cpp
+++ Codeforces/1385G Columns Swaps/std.cpp
@@ -4,7 +4,7 @@ void solve() {
 	int n;
 	std::cin >> n;
 	std::vector<int> a(n), b(n);
-	std::vector<std::vector<int>> E(n);
+	std::vector<std::vector<std::pair<int, int>>> E(n);
 	for (int i = 0; i < n; ++i) {
 		std::cin >> a[i];
 		--a[i];
@@ -15,8 +15,8 @@ void solve() {
 	}
 	std::vector<int> d(n);
 	for (int i = 0; i < n; ++i) {
-		E[a[i]].push_back(b[i]);
-		E[b[i]].push_back(a[i]);
+		E[a[i]].emplace_back(b[i], i);
+		E[b[i]].emplace_back(a[i], i);
 		++d[a[i]], --d[b[i]];
 	}
 	for (int i = 0; i < n; ++i) {
@@ -28,19 +28,48 @@ void solve() {
 	}
 	
 	std::vector<bool> vis(n);
+	std::vector<int> ans;
 	for (int i = 0; i < n; ++i) {
-		if (!vis[i]) {
-			int lst = E[i][1];
-			for (int j = i; !vis[j]; j = E[j][0]) {
-				E[i].erase(std::find(E[i].begin(), E[i].end(), lst));
-				
+		if (!vis[i] && d[i]) {
+			int lst = E[i][1].first;
+			int now = 0;
+			std::array<std::vector<int>, 2> id;
+			for (int j = i; !vis[j]; j = E[j][0].first) {
+				E[j].erase(std::find_if(E[j].begin(), E[j].end(), [&](std::pair<int, int> x) {
+					return x.first == lst;
+				}));
+				vis[j] = true;
+				lst = j;
+				if (d[j]) {
+					now ^= 1;
+				}
+				id[now].push_back(E[j][0].second);
+			}
+			if (now) {
+				std::cout << -1 << "\n";
+				return;
+			}
+			if (id[0].size() < id[1].size()) {
+				ans.insert(ans.end(), id[0].begin(), id[0].end());
+			} else {
+				ans.insert(ans.end(), id[1].begin(), id[1].end());
 			}
 		}
 	}
+	std::cout << ans.size() << "\n";
+	for (int x : ans) {
+		std::cout << x + 1 << " ";
+	}
+	std::cout << "\n";
 }
 
 int main() {
 	std::ios_base::sync_with_stdio(false);
 	std::cin.tie(0);
 
+	int T;
+	std::cin >> T;
+	while (T--) {
+		solve();
+	}
 }
