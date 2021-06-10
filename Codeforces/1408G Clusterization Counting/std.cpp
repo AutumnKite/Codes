cdd11270b3049db36e7541401025af268330d584
AutumnKite <1790397194@qq.com>
2021-06-10 17:03:19 +0800

    update

---
+++ Codeforces/1408G Clusterization Counting/std.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+
+int main() {
+	std::ios_base::sync_with_stdio(false);
+	std::cin.tie(0);
+
+	int n;
+	std::cin >> n;
+	std::vector<std::vector<int>> a(n, std::vector<int>(n));
+	
+}
