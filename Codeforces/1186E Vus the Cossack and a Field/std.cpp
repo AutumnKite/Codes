16c65e4b258146d950d2bfd01fd0def28a489235
AutumnKite <1790397194@qq.com>
2021-06-15 21:39:56 +0800

    update

--- Codeforces/1186E Vus the Cossack and a Field/std.cpp
+++ Codeforces/1186E Vus the Cossack and a Field/std.cpp
@@ -6,19 +6,38 @@ int main() {
 
 	int n, m, q;
 	std::cin >> n >> m >> q;
-	std::vector<std::vector<int>> s0(n + 1, std::vector<int>(m + 1)), s1(s0);
+	std::vector<std::vector<int>> s[2] = {
+		std::vector<std::vector<int>>(n + 1, std::vector<int>(m + 1)),
+		std::vector<std::vector<int>>(n + 1, std::vector<int>(m + 1))
+	};
 	for (int i = 0; i < n; ++i) {
 		std::string tmp;
 		std::cin >> tmp;
 		for (int j = 0; j < m; ++j) {
-			s0[i + 1][j + 1] = s0[i][j + 1] + s0[i + 1][j] - s0[i][j] + (tmp[j] == '1');
-			s1[i + 1][j + 1] = s1[i][j + 1] + s1[i + 1][j] - s1[i][j] + (tmp[j] == '0');
+			s[0][i + 1][j + 1] = s[0][i][j + 1] + s[0][i + 1][j] - s[0][i][j] + (tmp[j] == '1');
+			s[1][i + 1][j + 1] = s[1][i][j + 1] + s[1][i + 1][j] - s[1][i][j] + (tmp[j] == '0');
 		}
 	}
 
 	auto solve = [&](int x, int y) -> long long {
 		int cx = x / n, cy = y / m;
-		
+		long long ans = 0;
+		if (cx && cy) {
+			ans += 2ll * (cx / 2) * (cy / 2) * n * m;
+		    ans += 1ll * (cx & 1) * (cy / 2) * n * m;
+			ans += 1ll * (cx / 2) * (cy & 1) * n * m;
+			ans += 1ll * (cx & 1) * (cy & 1) * s[__builtin_parity((cx - 1) ^ (cy - 1))][n][m];
+		}
+		if (cy) {
+			ans += 1ll * (cy / 2) * (x % n) * m;
+			ans += 1ll * (cy & 1) * s[__builtin_parity(cx ^ (cy - 1))][x % n][m];
+		}
+		if (cx) {
+			ans += 1ll * (cx / 2) * n * (y % m);
+			ans += 1ll * (cx & 1) * s[__builtin_parity((cx - 1) ^ cy)][n][y % m];
+		}
+		ans += s[__builtin_parity(cx ^ cy)][x % n][y % m];
+		return ans;
 	};
 
 	while (q--) {
