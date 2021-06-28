b2d60734dcea4af56b5cd63420b24c6e381d5c8f
AutumnKite <1790397194@qq.com>
2021-06-28 17:48:52 +0800

    update

---
+++ Codeforces/1466H Finding satisfactory solutions/std.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+const int P = 1000000007;
+
+int main() {
+	std::ios_base::sync_with_stdio(false);
+	std::cin.tie(0);
+
+	int n;
+	std::cin >> n;
+	std::vector<int> p(n);
+	for (int i = 0; i < n; ++i) {
+		std::cin >> p[i];
+		--p[i];
+	}
+
+	
+}
