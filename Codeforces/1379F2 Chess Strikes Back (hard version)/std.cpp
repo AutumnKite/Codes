84d6512a961793b9bbfc2f183b2ef3d911042f94
AutumnKite <1790397194@qq.com>
2021-07-01 23:40:49 +0800

    update

---
+++ Codeforces/1379F2 Chess Strikes Back (hard version)/std.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+
+int main() {
+	std::ios_base::sync_with_stdio(false);
+	std::cin.tie(0);
+
+	int n, m, q;
+	std::cin >> n >> m >> q;
+	std::vector<std::set<int>> S(n);
+}
