7ca6202d58dcfe948c04ec7a87cac4640e4937c6
AutumnKite <1790397194@qq.com>
2021-07-20 16:28:03 +0800

    update

---
+++ Codeforces/1550D Excellent Arrays/std.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+
+const int P = 1000000007;
+
+std::vector<int> fac, inv;
+
+int qpow(int a, int b) {
+	int s = 1;
+	for (; b; b >>= 1) {
+		if (b & 1) {
+			s = 1ll * s * a % P;
+		}
+		a = 1ll * a * a % P;
+	}
+	return s;
+}
+
+void init(int n) {
+	fac.resize(n + 1), inv.resize(n + 1);
+	for (int i = 1; i <= n; ++i) {
+		fac[i] = 1ll * fac[i - 1] * i % P;
+	}
+	inv[n] = qpow(fac[n], P - 2);
+	for (int i = n; i >= 1; --i) {
+		inv[i - 1] = 1ll * inv[i] * i % P;
+	}
+}
+
+int C(int n, int m) {
+	if (m < 0 || m > n) {
+		return 0;
+	}
+	return 1ll * fac[n] * inv[m] % P * inv[n - m] % P;
+}
+
+void solve() {
+	int n, l, r;
+	std::cin >> n >> l >> r;
+	
+}
+
+int main() {
+	std::ios_base::sync_with_stdio(false);
+	std::cin.tie(0);
+
+	int T;
+	std::cin >> T;
+	while (T--) {
+		solve();
+	}
+}
