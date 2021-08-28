d58504b8d6fa9e3144410cdd59069afeab6cc0eb
AutumnKite <1790397194@qq.com>
2021-08-28 16:08:44 +0800

    update

---
+++ Codeforces/1553H XOR and Distance/std.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+int main() {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(0);
+
+    int k, n;
+    std::cin >> k >> n;
+    std::vector<int> a(n);
+    for (int i = 0; i < n; ++i) {
+        std::cin >> a[i];
+    }
+}
