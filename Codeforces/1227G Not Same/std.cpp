a1683fadc452084d1bd5a60b096c63048023f8d8
AutumnKite <1790397194@qq.com>
2021-09-09 23:27:09 +0800

    update

--- Codeforces/1227G Not Same/std.cpp
+++ Codeforces/1227G Not Same/std.cpp
@@ -12,13 +12,51 @@ int main() {
         a[i].second = i;
     }
     std::sort(a.begin(), a.end());
-    for (int i = 0; i < n; ++i) {
-        std::string s(n, '0');
-        for (int j = 0; j < n; ++j) {
-            if (a[i].first > (j > i)) {
-                --a[i].first;
-                s[a[i].second] = '1';
-            }
+    std::vector<std::string> ans;
+    for (int l = 0, r = 0; l < n; l = r) {
+        r = l;
+        while (r < n && a[r].first == a[l].first) {
+            ++r;
         }
+        if (a[l].first == n - l) {
+            std::string s(n, '0');
+            for (int j = 0; j < l; ++j) {
+                if (a[j].first > 0) {
+                    --a[j].first;
+                    s[a[j].second] = '1';
+                }
+            }
+            for (int j = l; j < r; ++j) {
+                --a[j].first;
+                s[a[j].second] = '1';
+            }
+            ans.emplace_back(s);
+        }
+        int len = std::min(r - l - 1, a[l].first);
+        for (int i = l; i < r; ++i) {
+            std::string s(n, '0');
+            for (int j = 0; j < l; ++j) {
+                if (a[j].first > 0) {
+                    --a[j].first;
+                    s[a[j].second] = '1';
+                }
+            }
+            for (int j = i, p = 0; p < len; ++p, j = (j - l + 1) % (r - l) + l) {
+                --a[j].first;
+                s[a[j].second] = '1';
+            }
+            for (int j = r; j < n; ++j) {
+                if (a[j].first > 1) {
+                    --a[j].first;
+                    s[a[j].second] = '1';
+                }
+            }
+            ans.push_back(s);
+        }
+    }
+
+    std::cout << ans.size() << "\n";
+    for (auto s : ans) {
+        std::cout << s << "\n";
     }
 }
