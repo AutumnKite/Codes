3c1502be18eedfce5b4d6aa42a0e2f5d4773f12c
AutumnKite <1790397194@qq.com>
2021-09-29 23:17:21 +0800

    update

--- Codeforces/1535E Gold Transfer/std.cpp
+++ Codeforces/1535E Gold Transfer/std.cpp
@@ -9,10 +9,7 @@ int main() {
     std::vector<int> a(q + 1), c(q + 1);
     int LG = std::__lg(q + 1) + 1;
     std::vector<std::vector<int>> fa(q + 1, std::vector<int>(LG, -1));
-    std::vector<std::vector<long long>> sum(q + 1, std::vector<long long>(LG));
-    std::vector<long long> sa(q + 1), sc(q + 1);
     std::cin >> a[0] >> c[0];
-    sc[0] = 1ll * a[0] * c[0];
     for (int i = 1; i <= q; ++i) {
         int op;
         std::cin >> op;
@@ -21,30 +18,42 @@ int main() {
             std::cin >> p;
             std::cin >> a[i] >> c[i];
             fa[i][0] = p;
-            sum[i][0] = a[i];
             for (int j = 1; j < LG && fa[i][j - 1] != -1; ++j) {
                 fa[i][j] = fa[fa[i][j - 1]][j - 1];
-                sum[i][j] = sum[i][j - 1] + sum[fa[i][j - 1]][j - 1];
             }
-            sc[i] = sc[p] + 1ll * a[i] * c[i];
-            sa[i] = sa[p] + a[i];
         } else {
-            int u;
-            std::cin >> u;
+            int x;
+            std::cin >> x;
             int w;
             std::cin >> w;
-            long long tw = sa[u] - w;
-            if (tw <= 0) {
-                std::cout << sc[u] << "\n";
-                continue;
-            }
+            int u = x, k = 0;
             for (int j = LG - 1; j >= 0; --j) {
-                if (fa[u][j] != -1 && sum[u][j] <= tw) {
-                    tw -= sum[u][j];
+                if (fa[u][j] != -1 && a[fa[u][j]]) {
                     u = fa[u][j];
+                    k += 1 << j;
                 }
             }
-            std::cout << sc[u] - tw * c[u] << "\n";
+            int now = 0;
+            long long ans = 0;
+            while (now < w && k >= 0) {
+                int u = x;
+                for (int j = 0; (1 << j) <= k; ++j) {
+                    if (k >> j & 1) {
+                        u = fa[u][j];
+                    }
+                }
+                if (now + a[u] <= w) {
+                    now += a[u];
+                    ans += 1ll * a[u] * c[u];
+                    a[u] = 0;
+                    --k;
+                } else {
+                    ans += 1ll * (w - now) * c[u];
+                    a[u] -= w - now;
+                    now = w;
+                }
+            }
+            std::cout << now << " " << ans << std::endl;
         }
     }
 }
