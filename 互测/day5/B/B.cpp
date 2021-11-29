749d6e9d43a81d56c3c3f9d4b13f14f4a410a7c5
AutumnKite <1790397194@qq.com>
2021-11-29 17:31:14 +0800

    update

--- 互测/day5/B/B.cpp
+++ 互测/day5/B/B.cpp
@@ -1,20 +1,59 @@
 #include <bits/stdc++.h>
 
-void solve(int l, int r) {
-  if (l + 1 == r) {
-    return;
+void solve1(int n) {
+  auto calc = [&](int i, int j) {
+    int k = (i + j) % (2 * n + 1);
+    if (k % 2 == 0) {
+      return k / 2;
+    } else {
+      return (k + 2 * n + 1) / 2;
+    }
+  };
+  auto ID = [&](int i, int c) {
+    return i * 3 + c + 1;
+  };
+  for (int i = 0; i < 2 * n + 1; ++i) {
+    std::cout << ID(i, 0) << " " << ID(i, 1) << " " << ID(i, 2) << "\n";
   }
-  int t = (r - l) / 3;
-  for (int i = 0; i < t; ++i) {
-    for (int j = 0; j < t; ++j) {
-      std::cout << i + l + 1 << " ";
-      std::cout << j + t + l + 1 << " ";
-      std::cout << (i + j) % t + 2 * t + l + 1 << "\n";
+  for (int i = 0; i < 2 * n + 1; ++i) {
+    for (int j = i + 1; j < 2 * n + 1; ++j) {
+      for (int k = 0; k < 3; ++k) {
+        std::cout << ID(i, k) << " " << ID(j, k) << " "
+                  << ID(calc(i, j), (k + 1) % 3) << "\n";
+      }
     }
   }
-  solve(l, l + t);
-  solve(l + t, r - t);
-  solve(r - t, r);
+}
+
+void solve2(int n) {
+  auto calc = [&](int i, int j) {
+    int k = (i + j) % (2 * n);
+    if (k % 2 == 0) {
+      return k / 2;
+    } else {
+      return (k + 2 * n - 1) / 2;
+    }
+  };
+  auto ID = [&](int i, int c) {
+    return i * 3 + c + 1;
+  };
+  for (int i = 0; i < n; ++i) {
+    std::cout << ID(i, 0) << " " << ID(i, 1) << " " << ID(i, 2) << "\n";
+  }
+  for (int i = 0; i < n; ++i) {
+    for (int k = 0; k < 3; ++k) {
+      std::cout << 6 * n + 1 << " " << ID(i, k) << " "
+                << ID(n + i, (k + 2) % 3) << "\n";
+    }
+  }
+  for (int i = 0; i < 2 * n; ++i) {
+    for (int j = i + 1; j < 2 * n; ++j) {
+      for (int k = 0; k < 3; ++k) {
+        std::cout << ID(i, k) << " " << ID(j, k) << " "
+                  << ID(calc(i, j), (k + 1) % 3) << "\n";
+      }
+    }
+  }
 }
 
 int main() {
@@ -23,5 +62,9 @@ int main() {
 
   int n;
   std::cin >> n;
-  solve(0, n);
+  if (n % 6 == 3) {
+    solve1(n / 6);
+  } else {
+    solve2(n / 6);
+  }
 }
--- 互测/day8/C/C.cpp
+++ 互测/day8/C/C.cpp
@@ -4,15 +4,36 @@ bool query(int);
 void modify(int, bool);
 
 bool WereYouLast(int n, int m) {
-  int p = 9;
-  while (p >= 0 && query(p + 1)) {
-    modify(p + 1, false);
-    --p;
+  if (n == 1024) {
+    int p = 9;
+    while (p >= 0 && query(p + 1)) {
+      modify(p + 1, false);
+      --p;
+    }
+    if (p == -1) {
+      return true;
+    } else {
+      modify(p + 1, true);
+      return false;
+    }
   }
-  if (p == -1) {
+
+  int x = 0;
+  for (int i = 0; i < 5; ++i) {
+    x |= query(i + 1) << i;
+  }
+  if ((1 << x) == n) {
     return true;
-  } else {
-    modify(p + 1, true);
-    return false;
   }
+  int now = query(x + 6);
+  modify(x + 6, !now);
+  if (now || 1 << x == n / 2) {
+    ++x;
+  } else {
+    x = 0;
+  }
+  for (int i = 0; i < 5; ++i) {
+    modify(i + 1, x >> i & 1);
+  }
+  return false;
 }
