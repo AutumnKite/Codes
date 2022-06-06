467a7096ed8dadff3ed6ee988cef5d7f8296e0d7
AutumnKite <1790397194@qq.com>
2022-06-06 14:00:13 +0800

    PR4

---
+++ PublicJudge/PR4/B/guess.h
@@ -0,0 +1,20 @@
+#include <string>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <random>
+#include <functional>
+using std::pair;
+using std::make_pair;
+using std::function;
+using std::vector;
+using std::string;
+using std::cout;
+using std::endl;
+using std::min;
+using std::max;
+using std::swap;
+
+vector<int> solve(int, int);
+bool query1(int, int);
+int query2(int, int, int);
