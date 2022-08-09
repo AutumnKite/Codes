2e48d3b623c13c16bc3f49125c69101d4d82020a
AutumnKite <1790397194@qq.com>
2022-08-09 18:05:20 +0800

    update

---
+++ 多校/hdu/day7/I/std.cpp
@@ -0,0 +1,191 @@
+#include <bits/stdc++.h>
+
+template<unsigned P>
+class modint {
+  static_assert(1 <= P, "P must be a positive integer");
+
+  using mint = modint<P>;
+
+protected:
+  unsigned v;
+
+public:
+  modint() : v() {}
+
+  template<typename T,
+           typename std::enable_if<std::is_integral<T>::value &&
+                                       std::is_signed<T>::value,
+                                   bool>::type = true>
+  modint(T t_v) : v() {
+    long long tmp = t_v % static_cast<long long>(P);
+    if (tmp < 0) {
+      tmp += P;
+    }
+    v = tmp;
+  }
+
+  template<typename T,
+           typename std::enable_if<std::is_integral<T>::value &&
+                                       std::is_unsigned<T>::value,
+                                   bool>::type = true>
+  modint(T t_v) : v() {
+    v = t_v % P;
+  }
+
+  unsigned val() const {
+    return v;
+  }
+
+  static unsigned mod() {
+    return P;
+  }
+
+  static mint raw(unsigned v) {
+    mint res;
+    res.v = v;
+    return res;
+  }
+
+  mint &operator+=(const mint &rhs) {
+    v < P - rhs.v ? v += rhs.v : v -= P - rhs.v;
+    return *this;
+  }
+
+  mint &operator++() {
+    v + 1 < P ? ++v : v = 0;
+    return *this;
+  }
+
+  mint operator++(int) {
+    mint tmp = *this;
+    v + 1 < P ? ++v : v = 0;
+    return tmp;
+  }
+
+  mint &operator-=(const mint &rhs) {
+    v < rhs.v ? v += P - rhs.v : v -= rhs.v;
+    return *this;
+  }
+
+  mint &operator--() {
+    v ? --v : v = P - 1;
+    return *this;
+  }
+  
+  mint operator--(int) {
+    mint tmp = *this;
+    v ? --v : v = P - 1;
+    return tmp;
+  }
+
+  mint operator-() const {
+    mint res;
+    res.v = v ? P - v : 0;
+    return res;
+  }
+
+  mint &operator*=(const mint &rhs) {
+    v = static_cast<unsigned long long>(v) * rhs.v % P;
+    return *this;
+  }
+
+  mint pow(unsigned long long b) const {
+    mint a(*this), s(1);
+    for (; b; b >>= 1) {
+      if (b & 1) {
+        s *= a;
+      }
+      a *= a;
+    }
+    return s;
+  }
+
+  mint inv() const {
+    return pow(P - 2);
+  }
+
+  friend mint operator+(const mint &lhs, const mint &rhs) {
+    return mint(lhs) += rhs;
+  }
+
+  friend mint operator-(const mint &lhs, const mint &rhs) {
+    return mint(lhs) -= rhs;
+  }
+
+  friend mint operator*(const mint &lhs, const mint &rhs) {
+    return mint(lhs) *= rhs;
+  }
+
+  friend bool operator==(const mint &lhs, const mint &rhs) {
+    return lhs.v == rhs.v;
+  }
+
+  friend bool operator!=(const mint &lhs, const mint &rhs) {
+    return lhs.v != rhs.v;
+  }
+
+  friend std::istream &operator>>(std::istream &in, mint &x) {
+    return in >> x.v;
+  }
+
+  friend std::ostream &operator<<(std::ostream &out, const mint &x) {
+    return out << x.v;
+  }
+};
+
+using mint = modint<1000000007>;
+
+
+int n, m;
+mint f[31], ifac[31];
+
+void init() {
+  ifac[0] = 1;
+  for (int i = 1; i <= 30; ++i) {
+    ifac[i] = ifac[i - 1] * mint(i).inv();
+  }
+  for (int i = 0; i <= 30; ++i) {
+    mint ways = 1;
+    for (int j = 1; j <= i + 1; ++j) {
+      ways *= n - j + 2;
+    }
+    f[i] = ways * mint(i + 1).inv();
+  }
+}
+
+mint ans;
+
+void dfs(int lim, int tot, int lst, mint ways) {
+  ans += ways * f[tot];
+  int i = lst + 1;
+  if (i > lim) {
+    return;
+  }
+  for (; 1ll * i * i <= lim; ++i) {
+    int pw = 1;
+    for (int j = 1; 1ll * pw * i <= lim; ++j) {
+      pw *= i;
+      dfs(lim / pw, tot + j, i, ways * ifac[j]);
+    }
+  }
+  ans += ways * mint::raw(lim - i + 1) * f[tot + 1];
+}
+
+int main() {
+  std::ios_base::sync_with_stdio(false);
+  std::cin.tie(nullptr);
+
+  int T;
+  std::cin >> T;
+  while (T--) {
+    std::cin >> n >> m;
+    init();
+    ans = 0;
+    dfs(m, 0, 1, 1);
+    std::cout << ans - 1 << "\n";
+  }
+}
+/*
+1
+1000000000 1000000000
+*/
