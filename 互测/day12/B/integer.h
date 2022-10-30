4cd9a4975559ff37cab14275d036c5579905e7d5
AutumnKite <1790397194@qq.com>
2022-10-30 13:55:24 +0800

    update

--- 互测/day12/B/integer.h
+++
@@ -1,6 +0,0 @@
-#ifndef __GRADER_INTEGER_H_
-#define __GRADER_INTEGER_H_
-#include <vector>
-int operate(const int i);
-std::vector<int> findPermutation(int n);
-#endif
---
+++ 互测2021/day12/B/integer.h
@@ -0,0 +1,6 @@
+#ifndef __GRADER_INTEGER_H_
+#define __GRADER_INTEGER_H_
+#include <vector>
+int operate(const int i);
+std::vector<int> findPermutation(int n);
+#endif
